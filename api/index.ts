3d83a2ba7ebe8abf4c96358b9b7c6f9696519d0c
Felipe Goncalves <lafelipegoncalves@MacBook-Pro-de-Felipe.local>
2022-04-24 19:04:20 -0300

    scaling up

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import os from 'os';
 
 const app = express();
 
 const color = process.env.IMAGE_COLOR || 'Blank';
+const hostname = os.hostname();
+const platform = os.platform();
 
 app.get('/', (req, res) => {
     return res.status(200).json({
-        message: `I\'m the ${color} image`
+        message: `I\'m the ${color} image`,
+        hostname,
+        platform
     });
 })
 
